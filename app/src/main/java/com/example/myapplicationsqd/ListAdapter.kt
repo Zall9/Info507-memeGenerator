ccff2594cab57497c2cca3c14f790ae43e3e25b8
Zall9 <paul.delifer@yahoo.fr>
2021-10-19 19:09:54 +0200

    Added editText to xmls
    @ToDo =>> add a button to show the recyclerView listing all memes

--- app/src/main/java/com/example/myapplicationsqd/ListAdapter.kt
+++ app/src/main/java/com/example/myapplicationsqd/ListAdapter.kt
@@ -4,25 +4,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.squareup.picasso.Picasso
+import org.w3c.dom.Text
 import java.util.*
 
-
-class DataAdapter(
+class ListAdapter (
     private val context: Context,
     private val ImgUrls: ArrayList<String>
 ) :
-    RecyclerView.Adapter<DataAdapter.ViewHolder>() {
+    RecyclerView.Adapter<ListAdapter.ViewHolder>() {
     override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
         val view = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.row_layout, viewGroup, false)
+            .inflate(R.layout.list_row_layout, viewGroup, false)
         return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, i: Int) {
+        holder.txt_android.text= ImgUrls.get(i)
 
-        Picasso.with(context).load(ImgUrls[i]).resize(600, 600).into(viewHolder.img_android)
     }
 
     override fun getItemCount(): Int {
@@ -31,11 +31,11 @@ class DataAdapter(
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-        var img_android: ImageView
+        var txt_android: TextView
 
         init {
-            img_android =
-                view.findViewById<View>(R.id.img_android) as ImageView
+            txt_android =
+                view.findViewById<View>(R.id.txt_android) as TextView
         }
     }
 
--- app/src/main/java/com/example/myapplicationsqd/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationsqd/MainActivity.kt
@@ -7,23 +7,39 @@ import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
-import java.io.File
 import java.util.*
-
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+    var textBottom: EditText?=null
+    var textUp: EditText?=null
+
     var ImgUrls: ArrayList<String> = ArrayList()
+    var Listmeme: ArrayList<String> = ArrayList()
+    var recyclerViewListMeme: RecyclerView? = null
+    var managerViewListMeme: LinearLayoutManager?=null
+    var adapterViewListMeme: DataAdapter?=null
     var recyclerView: RecyclerView? = null
-    var refreshView: SwipeRefreshLayout? = null
+    var bouttonGenerer: Button?=null
+    var showMemeListButton: Button?=null
     var Manager: LinearLayoutManager? = null
     var adapter: DataAdapter? = null
     var msg: String? = ""
     var lastMsg = ""
+    fun generateName() :String{
+        val charPool : List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+        val randomString = (1..15)
+            .map { i -> kotlin.random.Random.nextInt(0, charPool.size) }
+            .map(charPool::get)
+            .joinToString("");
+        return randomString + ".jpg"
+    }
     @SuppressLint("Range")
     fun downloadImage(url: String) {
         val directory = File(Environment.DIRECTORY_PICTURES)
@@ -41,7 +57,7 @@ class MainActivity : AppCompatActivity() {
                 .setDescription("")
                 .setDestinationInExternalPublicDir(
                     directory.toString(),
-                    File.separator + "MemeCreator" + File.separator + url.substring(url.lastIndexOf("/") + 1)
+                    File.separator + "MemeCreator" + File.separator + generateName()
                     //url.substring(url.lastIndexOf("/") + 1)
                 )
         }
@@ -60,7 +76,8 @@ class MainActivity : AppCompatActivity() {
                 msg = statusMessage(url, directory, status)
                 if (msg != lastMsg) {
                     this.runOnUiThread {
-                        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+
                     }
                     lastMsg = msg ?: ""
                 }
@@ -84,21 +101,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //creer_Json_local(ImgUrls);
-        //creer_Json_extern(ImgUrls);
-        ImgUrls.add("https://images.pexels.com/photos/2220401/pexels-photo-2220401.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350")
-        val sizeArray=ImgUrls.size
-        downloadImage(ImgUrls[0])
-        //for (i in 0 until sizeArray)
-        //{
-        //downloadImage(ImgUrls[i])
-        //}
-        refreshView= findViewById<SwipeRefreshLayout>(R.id.swipeRefresh)
-        //refreshView.setOnRefreshListener(MemeRequest(applicationContext, this))
-        recyclerView = findViewById<View>(R.id.card_recycler_view) as RecyclerView
+
+        bouttonGenerer= findViewById<Button>(R.id.bouttonSent)
+        textUp= findViewById<EditText>(R.id.textUp)
+        textBottom= findViewById<EditText>(R.id.textDown)
+
+        Listmeme.add("https://apimeme.com/meme?meme=10-Guy&top=Top+text&bottom=Bottom+text")
+        Listmeme.add("https://apimeme.com/meme?meme=1990s-First-World-Problems&top=Top+text&bottom=Bottom+text")
+        var textHaut= textUp!!.text
+        var textBas= textBottom!!.text
+
+    /*    showMemeListButton!!.setOnClickListener{
+            recyclerViewListMeme!!.setVisibility(View.VISIBLE)
+        }*/
+        ImgUrls.add("https://apimeme.com/meme?meme=Bonobo-Lyfe&top=${textHaut}&bottom=${textBas}")
+        bouttonGenerer!!.setOnClickListener{
+
+
+
+            //val sizeArray=ImgUrls.size
+            //downloadImage("https://apimeme.com/meme?meme=Bonobo-Lyfe&top=${textHaut}&bottom=${textBas}")
+
+        }
+        recyclerViewListMeme= findViewById<View>(R.id.listOfPictures) as RecyclerView
+        managerViewListMeme= LinearLayoutManager(this)
+        adapterViewListMeme= DataAdapter(this,Listmeme)
+        recyclerViewListMeme!!.layoutManager=managerViewListMeme
+        recyclerView = findViewById<View>(R.id.card_current_recycler_view) as RecyclerView
         Manager = LinearLayoutManager(this)
         recyclerView!!.layoutManager = Manager
         adapter = DataAdapter(this,ImgUrls)
         recyclerView!!.adapter = adapter
+
     }
 }
