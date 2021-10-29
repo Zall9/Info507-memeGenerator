e516952c9604667cfe15f88be6a7478f3aae9c9f
Zall9 <paul.delifer@yahoo.fr>
2021-10-29 10:47:25 +0200

    cc

--- app/src/main/java/com/example/myapplicationsqd/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationsqd/MainActivity.kt
@@ -1,127 +1,42 @@
 package com.example.myapplicationsqd
 import android.annotation.SuppressLint
-import android.app.Application
-import android.app.DownloadManager
-import android.content.Context
-import android.database.Cursor
-import android.graphics.Bitmap
-import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
-import android.util.Log
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.android.volley.Request
 import com.android.volley.RequestQueue
-import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
+import com.example.myapplicationsqd.adapter.DataAdapter
+import com.example.myapplicationsqd.adapter.ListMemeAdapter
+import com.example.myapplicationsqd.request.RequestVolley
 import com.squareup.picasso.Picasso
-import org.json.JSONException
-import java.io.ByteArrayOutputStream
-import java.util.*
-import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
 import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
-import android.graphics.drawable.BitmapDrawable
-import androidx.core.content.FileProvider
-import androidx.core.graphics.drawable.toBitmap
-
 
 class MainActivity : AppCompatActivity() {
     lateinit var imageViews: ImageView
-    lateinit var bitMap: Bitmap
-    lateinit var memeSelected: String
-    lateinit var bos : ByteArrayOutputStream
+
     var toptext: EditText?=null
     var bottomtext: EditText?=null
-    var ImgUrls: ArrayList<String> = ArrayList()
     var Listmeme: HashMap<String,String> = hashMapOf()
     var recyclerViewListMeme: RecyclerView? = null
     var managerViewListMeme: LinearLayoutManager?=null
     var adapterViewListMeme: ListMemeAdapter?=null
-    var recyclerView: RecyclerView? = null
     var bouttonGenerer: Button?=null
-    var shareButton: ImageButton?=null
     var showMemeListButton: Button?=null
-    var Manager: LinearLayoutManager? = null
-    var adapter: DataAdapter? = null
     var msg: String? = ""
     var lastMsg = ""
     var requestQueue: RequestQueue? = null
     var list_row_layout: RelativeLayout?=null
+    var Manager: LinearLayoutManager? = null
+    var adapter: DataAdapter? = null
+    var ImgUrls: ArrayList<String> = ArrayList()
+    var recyclerView: RecyclerView? = null
+    //var shareButton: ImageButton?=null
 
-    fun generateName() :String{
-        val charPool : List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
-        val randomString = (1..15)
-            .map { i -> kotlin.random.Random.nextInt(0, charPool.size) }
-            .map(charPool::get)
-            .joinToString("");
-        println(randomString + ".jpg")
-        return randomString + ".jpg"
-    }
-    @SuppressLint("Range")
-    fun downloadImage(url: String) {
-        val directory = File(Environment.DIRECTORY_PICTURES)
-        if (!directory.exists()) {
-            directory.mkdirs()
-        }
-        val downloadManager = this.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-        //Environment.getStorageDirectory()
-        val downloadUri = Uri.parse(url)
 
-        val request = DownloadManager.Request(downloadUri).apply {
-            setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
-                .setAllowedOverRoaming(false)
-                .setTitle(url.substring(url.lastIndexOf("/") + 1))
-                .setDescription("")
-                .setDestinationInExternalPublicDir(
-                    directory.toString(),
-                    File.separator + "MemeCreator" + File.separator + generateName()
-                    //url.substring(url.lastIndexOf("/") + 1)
-                )
-        }
-
-        val downloadId = downloadManager.enqueue(request)
-        val query = DownloadManager.Query().setFilterById(downloadId)
-        Thread(Runnable {
-            var downloading = true
-            while (downloading) {
-                val cursor: Cursor = downloadManager.query(query)
-                cursor.moveToFirst()
-                if (cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS)) == DownloadManager.STATUS_SUCCESSFUL) {
-                    downloading = false
-                }
-                val status = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
-                msg = statusMessage(url, directory, status)
-                if (msg != lastMsg) {
-                    this.runOnUiThread {
-                        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
-
-                    }
-                    lastMsg = msg ?: ""
-                }
-                cursor.close()
-            }
-        }).start()
-    }
-    private fun statusMessage(url: String, directory: File, status: Int): String? {
-        msg = when (status) {
-            DownloadManager.STATUS_FAILED -> "Download has been failed, please try again"
-            DownloadManager.STATUS_PAUSED -> "Paused"
-            DownloadManager.STATUS_PENDING -> "Pending"
-            DownloadManager.STATUS_RUNNING -> "Downloading..."
-            DownloadManager.STATUS_SUCCESSFUL -> "Image downloaded successfully in $directory" + File.separator + url.substring(
-                url.lastIndexOf("/") + 1
-            )
-            else -> "There's nothing to download"
-        }
-        return msg
-    }
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -143,7 +58,7 @@ class MainActivity : AppCompatActivity() {
         var textBas= bottomtext!!.text
 
         showMemeListButton!!.setOnClickListener {
-            jsonParse()
+            RequestVolley.jsonParse(requestQueue!!, Listmeme)
 
             if (recyclerViewListMeme!!.visibility==View.VISIBLE){
                 recyclerViewListMeme!!.visibility= View.GONE
@@ -164,7 +79,7 @@ class MainActivity : AppCompatActivity() {
                 for ((key, value) in Listmeme){
                     list.add(value)
                 }
-                //val list : ArrayList<String> = Listmeme.values as ArrayList<String>
+
                 val memeSelected=list.get(recyclerViewListMeme!!.getChildViewHolder(view).adapterPosition)
                 Picasso.with(applicationContext).load(API_URL.format(memeSelected, "TOP", "BOTTOM")).resize(600, 600).into(imageViews)
 
@@ -173,7 +88,8 @@ class MainActivity : AppCompatActivity() {
                     for ((key, value) in Listmeme){
                         listurl.add(key)
                     }
-                    downloadImage(API_URL.format(memeSelected, textHaut, textBas))
+
+                    com.example.myapplicationsqd.request.DownloadManager.downloadImage(API_URL.format(memeSelected, textHaut, textBas),applicationContext,this@MainActivity)
                 }
 
 
@@ -216,26 +132,5 @@ class MainActivity : AppCompatActivity() {
 
 
 
-    private fun jsonParse() {
-        val url = "http://os-vps418.infomaniak.ch:1186/i507_1_2/listmeme.json"
-        val request = JsonObjectRequest(Request.Method.GET, url, null,
-            { response ->
-                try {
-                    val iter = response.keys()
-                    while (iter.hasNext()) {
-                        val key = iter.next()
-                        try {
-                            val value = response.get(key) as String
-                            Listmeme[key] = value
-                            Log.d("tablo", "$key")
-                        } catch (e: JSONException){
-                            e.printStackTrace()
-                        }
-                    }
-                } catch (e: JSONException) {
-                    e.printStackTrace()
-                }
-            }, { error -> error.printStackTrace() })
-        requestQueue?.add(request)
-    }
+
 }
--- app/src/main/java/com/example/myapplicationsqd/adapter/ListMemeAdapter.kt
+++ app/src/main/java/com/example/myapplicationsqd/adapter/ListMemeAdapter.kt
@@ -1,4 +1,4 @@
-package com.example.myapplicationsqd
+package com.example.myapplicationsqd.adapter
 
 import android.content.Context
 import android.view.LayoutInflater
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplicationsqd.R
 import kotlin.collections.HashMap
 
 abstract class ListMemeAdapter(
