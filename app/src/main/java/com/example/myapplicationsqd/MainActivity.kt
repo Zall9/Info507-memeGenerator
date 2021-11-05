f93ac5eda86c512c440349462713c22b4ee45961
Zall9 <paul.delifer@yahoo.fr>
2021-11-05 15:49:17 +0100

    Visibility set ListMemerecyclerView gone or visible by button press

--- app/src/main/java/com/example/myapplicationsqd/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationsqd/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.example.myapplicationsqd
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.graphics.drawable.toBitmap
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.RequestQueue
@@ -13,8 +20,15 @@ import com.example.myapplicationsqd.adapter.ListMemeAdapter
 import com.example.myapplicationsqd.request.DownloadManager
 import com.example.myapplicationsqd.request.RequestVolley
 import com.squareup.picasso.Picasso
+import java.io.ByteArrayOutputStream
 import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
+import android.graphics.drawable.BitmapDrawable
+
+import android.R.attr.path
+
+
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var imageViews: ImageView
@@ -25,17 +39,11 @@ class MainActivity : AppCompatActivity() {
     var recyclerViewListMeme: RecyclerView? = null
     var managerViewListMeme: LinearLayoutManager?=null
     var adapterViewListMeme: ListMemeAdapter?=null
+    var bouttonTelecharger: Button?=null
     var bouttonGenerer: Button?=null
     var showMemeListButton: Button?=null
-    var msg: String? = ""
-    var lastMsg = ""
     var requestQueue: RequestQueue? = null
-    var list_row_layout: RelativeLayout?=null
-    var Manager: LinearLayoutManager? = null
-    var adapter: DataAdapter? = null
-    var ImgUrls: ArrayList<String> = ArrayList()
-    var recyclerView: RecyclerView? = null
-    //var shareButton: ImageButton?=null
+    var shareButton: ImageButton?=null
 
 
     @SuppressLint("ClickableViewAccessibility")
@@ -49,14 +57,17 @@ class MainActivity : AppCompatActivity() {
         val API_URL = "http://apimeme.com/meme?meme=%s&top=%s&bottom=%s"
 
 
-
-        bouttonGenerer= findViewById<Button>(R.id.bouttonSent)
+        shareButton=findViewById<ImageButton>(R.id.sharebutton)
+        bouttonGenerer= findViewById<Button>(R.id.bouttonGenerate)
+        bouttonTelecharger=findViewById<Button>(R.id.bouttonSent)
         showMemeListButton= findViewById<Button>(R.id.ButtonShowListOfMeme)
         toptext= findViewById<EditText>(R.id.textUp)
         bottomtext= findViewById<EditText>(R.id.textDown)
 
         var textHaut= toptext!!.text
         var textBas= bottomtext!!.text
+
+
 
         showMemeListButton!!.setOnClickListener {
             RequestVolley.jsonParse(requestQueue!!, Listmeme)
@@ -82,8 +93,10 @@ class MainActivity : AppCompatActivity() {
 
                 val memeSelected=list.get(recyclerViewListMeme!!.getChildViewHolder(view).adapterPosition)
                 Picasso.with(applicationContext).load(API_URL.format(memeSelected, "TOP", "BOTTOM")).resize(600, 600).into(imageViews)
-
-                bouttonGenerer!!.setOnClickListener{
+                bouttonGenerer!!.setOnClickListener {
+                    Picasso.with(applicationContext).load(API_URL.format(memeSelected, textHaut, textBas)).resize(550,600).into(imageViews)
+                }
+                bouttonTelecharger!!.setOnClickListener{
                     var listurl : ArrayList<String> = arrayListOf()
                     for ((key, value) in Listmeme){
                         listurl.add(key)
@@ -91,42 +104,23 @@ class MainActivity : AppCompatActivity() {
 
                     com.example.myapplicationsqd.request.DownloadManager.downloadImage(API_URL.format(memeSelected, textHaut, textBas),applicationContext,this@MainActivity)
                 }
-
-
                 Toast.makeText(applicationContext, "$memeSelected", Toast.LENGTH_LONG).show()
-//                recyclerView = findViewById<View>(R.id.card_current_recycler_view) as RecyclerView
-  //              Manager = LinearLayoutManager(applicationContext)
-    //            recyclerView!!.layoutManager = Manager
-      //          adapter = DataAdapter(applicationContext,ImgUrls)
-        //        recyclerView!!.adapter = adapter
+                shareButton!!.setOnClickListener{
+                    val bitmap = (imageViews.getDrawable() as BitmapDrawable).bitmap
+                    val intent=Intent()
+                    intent.action=Intent.ACTION_SEND
+                    val path= MediaStore.Images.Media.insertImage(contentResolver,bitmap,"title","null")
+                    val uri=Uri.parse(path)
+                    intent.putExtra(Intent.EXTRA_STREAM, uri)
+                    intent.type="image/*"
+                    startActivity(Intent.createChooser(intent,"Share to"))
+                }
                 recyclerViewListMeme!!.visibility= View.GONE
 
                 return true
                                                         }
             }
         recyclerViewListMeme!!.adapter = adapterViewListMeme
-
-        /*shareButton!!.setOnClickListener {
-            val bitmap = imageViews.drawable
-            //val bitMap : Bitmap =imageViews.getDrawingCache()
-            val bos : ByteArrayOutputStream = ByteArrayOutputStream()
-
-            val directory: File= File(cacheDir,"images")
-            try{
-                directory.mkdir()
-                var file: File = File(directory,"image_partagee.jpg")
-                var outputStream: FileOutputStream= FileOutputStream(file)
-                bitMap.compress(Bitmap.CompressFormat.JPEG, 100, bos)
-                outputStream.flush()
-                outputStream.close()
-                val uri = FileProvider.getUriForFile(applicationContext, "com.example.myapplicationsqd", file);
-            }catch (e:Exception){}
-
-        }*/
-
-
-
-        //RecyclerView qui affiche le meme selectionné
 
     }
 
